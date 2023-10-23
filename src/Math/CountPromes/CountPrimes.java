e128dc664ac2baa987e3d5c59034d246c73ca9b9
Dennis <57582915+Dennis824@users.noreply.github.com>
2023-10-23 06:50:00 -0600

    CountPrimes

---
+++ src/Math/CountPromes/CountPrimes.java
@@ -0,0 +1,20 @@
+package Math.CountPromes;
+
+public class CountPrimes {
+    public int countPrimes(int n) {
+        boolean[] seen = new boolean[n];
+        int ans = 0;
+        for (int num = 2; num < n; num++) {
+            if (seen[num]) continue;
+            ans += 1;
+            for (long mult = (long)num * num; mult < n; mult += num)
+                seen[(int)mult] = true;
+        }
+        return ans;
+    }
+}
+/*
+204
+TC O(n * log(log n))
+SC O(n)
+*/
