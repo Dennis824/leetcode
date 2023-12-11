8f9f81bbd372097686bb02400a583eb084bde41f
Dennis <57582915+Dennis824@users.noreply.github.com>
2023-12-11 08:38:03 -0600

    ElementAppearingMoreThan25InSortedArray1287

---
+++ src/ElementAppearingMoreThan25InSortedArray1287/ElementAppearingMoreThan25InSortedArray1287.java
@@ -0,0 +1,19 @@
+package ElementAppearingMoreThan25InSortedArray1287;
+
+public class ElementAppearingMoreThan25InSortedArray1287 {
+    public int findSpecialInteger(int[] arr) {
+        Map<Integer, Integer> counts = new HashMap();
+        for (int num : arr) {
+            counts.put(num, counts.getOrDefault(num, 0) + 1);
+        }
+
+        int target = arr.length / 4;
+        for (int key : counts.keySet()) {
+            if (counts.get(key) > target) {
+                return key;
+            }
+        }
+
+        return -1;
+    }
+}
