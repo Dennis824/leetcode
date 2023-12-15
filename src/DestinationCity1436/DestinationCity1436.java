1a4bebb58b7e7b8e8400799d88ccb28aba64af83
Dennis <57582915+Dennis824@users.noreply.github.com>
2023-12-15 12:52:07 -0600

    DestinationCity1436

---
+++ src/DestinationCity1436/DestinationCity1436.java
@@ -0,0 +1,23 @@
+package DestinationCity1436;
+
+class DestinationCity1436 {
+    public String destCity(List<List<String>> paths) {
+        for (int i = 0; i < paths.size(); i++) {
+            String candidate = paths.get(i).get(1);
+            boolean good = true;
+
+            for (int j = 0; j < paths.size(); j++) {
+                if (paths.get(j).get(0).equals(candidate)) {
+                    good = false;
+                    break;
+                }
+            }
+
+            if (good) {
+                return candidate;
+            }
+        }
+
+        return "";
+    }
+}
