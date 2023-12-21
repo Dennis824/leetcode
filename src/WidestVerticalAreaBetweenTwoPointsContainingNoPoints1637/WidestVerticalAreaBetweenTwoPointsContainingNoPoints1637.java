2472c852ff38c1944f0a999be2146589a646ad78
Dennis <57582915+Dennis824@users.noreply.github.com>
2023-12-21 14:15:47 -0600

    WidestVerticalAreaBetweenTwoPointsContainingNoPoints1637

---
+++ src/WidestVerticalAreaBetweenTwoPointsContainingNoPoints1637/WidestVerticalAreaBetweenTwoPointsContainingNoPoints1637.java
@@ -0,0 +1,14 @@
+package WidestVerticalAreaBetweenTwoPointsContainingNoPoints1637;
+
+class WidestVerticalAreaBetweenTwoPointsContainingNoPoints1637{
+    public int maxWidthOfVerticalArea(int[][] points) {
+        Arrays.sort(points, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int ans = 0;
+        for (int i = 1; i < points.length; i++) {
+            ans = Math.max(ans, points[i][0] - points[i - 1][0]);
+        }
+
+        return ans;
+    }
+}
