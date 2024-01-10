f266ce3b272371c750036e7ce1558d275c127a33
Dennis <57582915+Dennis824@users.noreply.github.com>
2024-01-09 21:13:54 -0600

    AmountOfTimeForBinaryTreeToBeInfected2385

---
+++ src/AmountOfTimeForBinaryTreeToBeInfected2385/AmountOfTimeForBinaryTreeToBeInfected2385.java
@@ -0,0 +1,34 @@
+package AmountOfTimeForBinaryTreeToBeInfected2385;
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+
+class AmountOfTimeForBinaryTreeToBeInfected2385{
+
+    public void convert(TreeNode current, int parent, Map<Integer, Set<Integer>> map){
+        if (current == null) {
+            return;
+        }
+        if (!map.containsKey(current.val)) {
+            map.put(current.val, new HashSet<>());
+        }
+        Set<Integer> adjacentList = map.get(current.val);
+        if (parent != 0) {
+            adjacentList.add(parent);
+        }
+        if (current.left != null) {
+            adjacentList.add(current.left.val);
+        }
+        if (current.right != null) {
+            adjacentList.add(current.right.val);
+        }
+        convert(current.left, current.val, map);
+        convert(current.right, current.val, map);
+    }
