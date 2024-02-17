b4517341df1a91cfee6b669fa574b58e15991976
Dennis <57582915+Dennis824@users.noreply.github.com>
2024-02-17 17:49:09 -0600

    FurthestBuildingYouCanReach1642

---
+++ src/FurthestBuildingYouCanReach1642/FurthestBuildingYouCanReach1642.java
@@ -0,0 +1,22 @@
+package FurthestBuildingYouCanReach1642;
+
+class FurthestBuildingYouCanReach1642{
+    public int furthestBuilding(int[] A, int bricks, int ladders) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < A.length - 1; i++) {
+            int d = A[i + 1] - A[i];
+            if (d > 0)
+                pq.add(d);
+            if (pq.size() > ladders)
+                bricks -= pq.poll();
+            if (bricks < 0)
+                return i;
+        }
+        return A.length - 1;
+    }
+}
+
+/*
+Time O(NlogK)
+Space O(K)
+*/
