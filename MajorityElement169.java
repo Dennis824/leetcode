c785ebdf6abb71930e4df5279ec4f9f4f72bd741
Dennis <57582915+Dennis824@users.noreply.github.com>
2024-03-13 09:40:58 -0600

    refactor

--- MajorityElement169.java
+++
@@ -1,9 +0,0 @@
-package MajorityElement169;
-
-class MajorityElement169{
-    public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        return nums[n/2];
-    }
-}
--- MaximumProductDifferenceBetweenTwoPairs1913/MaximumProductDifferenceBetweenTwoPairs1913.java
+++
@@ -1,8 +0,0 @@
-package MaximumProductDifferenceBetweenTwoPairs1913;
-
-class MaximumProductDifferenceBetweenTwoPairs1913 {
-    public int maxProductDifference(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length - 1] * nums[nums.length - 2] - nums[0] * nums[1];
-    }
-}
