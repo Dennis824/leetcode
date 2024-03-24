89498b4e741a9c41fe49021ecb4e1267882f39cd
Dennis <57582915+Dennis824@users.noreply.github.com>
2024-03-24 05:13:23 -0600

    refactor

--- RepeatedSubstringPattern459/RepeatedSubstringPattern459.java
+++
@@ -1,9 +0,0 @@
-package RepeatedSubstringPattern459;
-
-class RepeatedSubstringPattern459{
-    public boolean repeatedSubstringPattern(String s) {
-        String doubled = s + s;
-        String sub = doubled.substring(1, doubled.length() - 1);
-        return sub.contains(s);
-    }
-}
