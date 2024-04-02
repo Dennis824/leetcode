40498fc86b45759cf97079f5960a274112dfe3c0
Dennis <57582915+Dennis824@users.noreply.github.com>
2024-04-02 06:34:35 -0600

    refactor

---
+++ src/Array/BestTimetoBuyAndSellStockII/BestTimetoBuyAndSellStockII.java
@@ -0,0 +1,23 @@
+package Array.BestTimetoBuyAndSellStockII;
+
+public class BestTimetoBuyAndSellStockII122 {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length <= 1) {
+            return 0;
+        }
+
+        int totalProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i - 1] < prices[i]) {
+                totalProfit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return totalProfit;
+    }
+}
+/*
+122
+Time complexity:O(n)
+Space complexity:O(1)
+*/
