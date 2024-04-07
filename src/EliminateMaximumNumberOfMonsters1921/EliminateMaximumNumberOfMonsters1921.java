07f73c07d2a840cd940c15cb9196f4ebe03899f3
Dennis <57582915+Dennis824@users.noreply.github.com>
2024-04-07 15:33:34 -0600

    refactor

---
+++ src/EliminateMaximumNumberOfMonsters1921/EliminateMaximumNumberOfMonsters1921.java
@@ -0,0 +1,25 @@
+package EliminateMaximumNumberOfMonsters1921;
+
+import java.util.Arrays;
+
+public class EliminateMaximumNumberOfMonsters1921 {
+    public int eliminateMaximum(int[] dist, int[] speed) {
+        double[] arrival = new double[dist.length];
+        for (int i = 0; i < dist.length; i++) {
+            arrival[i] = (double) dist[i] / speed[i];
+        }
+
+        Arrays.sort(arrival);
+        int ans = 0;
+
+        for (int i = 0; i < arrival.length; i++) {
+            if (arrival[i] <= i) {
+                break;
+            }
+
+            ans++;
+        }
+
+        return ans;
+    }
+}
