ee0d15a151de50f5a4cb778808216040957ff2ef
Dennis <57582915+Dennis824@users.noreply.github.com>
2024-04-09 09:33:00 -0600

    refactor

---
+++ src/FindTheWinnerOfAnArrayGame1535/FindTheWinnerOfAnArrayGame1535.java
@@ -0,0 +1,36 @@
+package FindTheWinnerOfAnArrayGame1535;
+
+class FindTheWinnerOfAnArrayGame1535
+{
+    public int getWinner(int[] arr, int k) {
+        int maxElement = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            maxElement = Math.max(maxElement, arr[i]);
+        }
+
+        int curr = arr[0];
+        int winstreak = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+            int opponent = arr[i];
+
+            if (curr > opponent) {
+                winstreak++;
+            } else {
+                curr = opponent;
+                winstreak = 1;
+            }
+
+            if (winstreak == k || curr == maxElement) {
+                return curr;
+            }
+        }
+
+        return -1;
+    }
+}
+/*
+1535
+TC O(n)
+SC O(1)
+ */
