a263eb73ab6776cff3afb518fdd07e85038eb205
Dennis <57582915+Dennis824@users.noreply.github.com>
2024-05-01 06:50:28 -0600

    refactor

---
+++ src/DynamicProgramming/MaximumSubarray/MaximumSubarray.java
@@ -0,0 +1,18 @@
+package DynamicProgramming.MaximumSubarray;
+
+class MaximumSubarray53 {
+    public int maxSubArray(int[] nums) {
+        int sum = nums[0];
+        int max_sum = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            sum = Math.max(nums[i] + sum, nums[i]);
+            max_sum = Math.max(max_sum, sum);
+        }
+        return max_sum;
+    }
+}
+/*
+53
+TC On
+SC O1
+*/
