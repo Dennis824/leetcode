6f677e753c29ffb7042e06594ce1a64ccf6deb60
Dennis <57582915+Dennis824@users.noreply.github.com>
2024-05-07 05:30:35 -0600

    refactor

---
+++ src/MinimumNumberOfOperationsToMakeArrayEmpty2870/MinimumNumberOfOperationsToMakeArrayEmpty2870.java
@@ -0,0 +1,18 @@
+package MinimumNumberOfOperationsToMakeArrayEmpty2870;
+
+class MinimumNumberOfOperationsToMakeArrayEmpty2870{
+    public int minOperations(int[] nums) {
+        var counter = new HashMap<Integer, Integer>();
+        for (int num: nums) {
+            counter.put(num, counter.getOrDefault(num, 0) + 1);
+        }
+        int ans = 0;
+        for (int c: counter.values()) {
+            if (c == 1) {
+                return -1;
+            }
+            ans += Math.ceil((double) c / 3);
+        }
+        return ans;
+    }
+}
