f70794912bc99cd4a6ccdd187fbbb45efacc5868
Dennis <57582915+Dennis824@users.noreply.github.com>
2024-05-24 07:14:38 -0600

    refactor

---
+++ src/LinkedList/ReverseLinkedList/ReverseLinkedList.java
@@ -0,0 +1,19 @@
+package LinkedList.ReverseLinkedList;
+
+public class ReverseLinkedList206 {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while(head != null){
+            ListNode next_node = head.next;
+            head.next = prev;
+            prev = head;
+            head = next_node;
+        }
+        return prev;
+    }
+}
+/*
+206
+TC O(n)
+SC O(1)
+ */
